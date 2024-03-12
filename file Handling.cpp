38ad470edd49e92ded6b18137cc88e4ae114c129
Om Dhormare <143721889+omdhormare@users.noreply.github.com>
2024-03-12 22:36:35 +0530

    Add files via upload

---
+++ file Handling.cpp
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<iostream.h>
+#include<fstream.h>
+#include<ctype.h>
+int main()
+{
+    clrscr();
+    fstream f1("student.txt",ios::out);
+    f1<<"omdadaruhgsiekjnuizhgrnsejkh8fiuakjwnluiershgnuejkatha8i4us";
+    f1.close();
+   cout<<"Succcefully Push......";
+   getch();
+}
