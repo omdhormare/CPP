96eef1f3855e7a6ecd8563f532fd4dc434b6ccd1
Om Dhormare <143721889+omdhormare@users.noreply.github.com>
2024-03-25 13:05:37 +0530

    Add files via upload

--- slip no 11.2.cpp
+++ slip no 11.2.cpp
@@ -1,141 +1,113 @@
-#include<iostream>
-using namespace std;
-
-class student 
-{
-public:
+ #include<conio.h>
+ #include<iostream.h>
+ class student
+ {
+   protected:
     int rno;
-    char sname[20];
-
-    void accept()
+    char name[30];
+   public:
+    void acceptstudent()
     {
-        cout << "\n Enter Student Roll No : ";
-        cin >> rno;
-        cout << "\n Enter Student Name : ";
-        cin >> sname;
+      cout<<"Enter Roll No : ";
+      cin>>rno;
+      cout<<"\nEnter Student Name : ";
+      cin>>name;
     }
-};
-
-class theory
-{
-public:
-    int M1, M2, M3, M4;
-
+    void diss()
+    {
+      cout<<endl<<"-------------------------------------------------"<<endl;
+      cout<<"\nRoll No :"<<rno;
+      cout<<"\nStudent Name: "<<name;
+    }
+ };
+ class theory:virtual public student
+ {
+   protected:
+    int m1,m2,m3,m4;
+   public:
     void acceptT()
     {
-        cout << "\n Enter M1 Mark : ";
-        cin >> M1;
-        cout << "\n Enter M2 Mark : ";
-        cin >> M2;
-        cout << "\n Enter M3 Mark : ";
-        cin >> M3;
-        cout << "\n Enter M4 Mark : ";
-        cin >> M4;
+      cout<<endl<<"-------------------------------------------------"<<endl;
+      cout<<"Enter M1 Mark : ";
+      cin>>m1;
+      cout<<"\nEnter M2 Mark: ";
+      cin>>m2;
+      cout<<"\nEnter M3 Mark : ";
+      cin>>m3;
+      cout<<"\nEnter M4 Mark: ";
+      cin>>m4;
     }
-};
-
-class practical
-{
-public:
-    int p1, p2;
-
+ };
+  class practical:virtual public student
+ {
+   protected:
+    int p1,p2;
+   public:
     void acceptP()
     {
-        cout << "\n Enter P1 Mark : ";
-        cin >> p1;
-        cout << "\n Enter P2 Mark : ";
-        cin >> p2;
+      cout<<"Enter P1 Mark : ";
+      cin>>p1;
+      cout<<"\nEnter p2 Mark: ";
+      cin>>p2;
     }
-};
-
-class result : public student, public theory, public practical
-{
-public:
-    int total;
+ };
+ class result: public theory,public practical
+ {
+   protected:
+    int total_mark;
     float per;
-    char grade;
-
+   public:
     void cal()
     {
-        total = M1 + M2 + M3 + M4 + p1 + p2;
-        per = float(total) / 6;
+       total_mark=p1+p2+m3+m4+m1+m2;
+       per=total_mark/6;
+       cout<<endl<<"-------------------------------------------------"<<endl;
+       cout<<"\nTotal Mark :"<<total_mark;
+       cout<<"\nPercentage : "<<per<<"%";
+       cout<<"\nGrade : ";
+       if(per>90)
+       cout<<"O";
+       else if(per>75)
+       cout<<"A+";
+       else if(per>60)
+       cout<<"A";
+       else if(per>45)
+       cout<<"B";
+       else if(per>40)
+       cout<<"P";
+       else
+	cout<<"F";
+       //cout<<"\nGrade : ";
     }
-
-    void cal_grade()
-	{
-		if(per>=90)
-		{
-			cout<<"\n Grade : O";
-		}
-		else if(per>=85)
-		{
-			cout<<"\n Grade : A+";
-		}
-		else if(per>=70)
-		{
-			cout<<"\n Grade : A";
-		}
-		else if(per>=50)
-		{
-			cout<<"\n Grade : B+";
-		}
-		else if(per>=40)
-		{
-			cout<<"\n Grade : PASS";
-		}
-		else 
-		{
-			cout<<"\n Grade : Fail";
-		}
-	}
-
-
-    void dis()
-    {
-        cout << "\n Roll No : " << rno;
-        cout << "\n Student Name :" << sname;
-        cout << "\n Total Mark : " << total;
-        cout << "\n Percentage : " << per;
-    }
-    void disS()
-    {
-    	 cout << "\n Roll No : " << rno;
-        cout << "\n Student Name :" << sname;
-	}
-};
-
-int main()
-{
+ };
+ void main()
+ {
     int ch;
     result ob;
+    clrscr();
+    do{
+	cout<<endl<<"-----------------------------------------------"<<endl;
+	cout<<"\n1.Accept Student Information : ";
+	cout<<"\n2.Display Student Infoamtion :";
+	cout<<"\n3.Calculate Total Mark,per,grade ";
+	cout<<endl<<"-------------------------------------------------"<<endl;
+	cout<<"\nEnter A choice : ";
+	cin>>ch;
+	switch(ch)
+	{
+	  case 1:ob.acceptstudent();
+		 break;
+	  case 2:ob.diss();
+		 break;
+	  case 3:ob.acceptT();
+		 ob.acceptP();
+		 ob.cal();
+		 break;
+	}
+      }while(ch<4);
+      getch();
+ }
 
-    do
-    {
-        cout << "\n .....MENU.....\n ";
-        cout << "\n 1. Accept Student Information..";
-        cout << "\n 2. Calculate Total _ marks, Percentage and Grade....";
-        cout << "\n 3. Display Student Information...";
-        cout << "\n\n ...............................................................................\n\n";
-        cout << "\n Enter The Choice : ";
-        cin >> ch;
 
-        switch (ch)
-        {
-        case 1:
-            ob.accept();
-            ob.acceptT();
-            ob.acceptP();
-            break;
-        case 2:
-            ob.cal();
-            ob.cal_grade();
-            ob.dis();
-            break;
-        case 3:
-            ob.disS();
-            break;
-        }
-    } while (ch < 4);
 
-}
 
